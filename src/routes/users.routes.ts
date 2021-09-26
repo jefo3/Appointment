a22a98d588f53a619f9d0e6272af84894dbfbc4b
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2021-09-25 22:22:58 -0300

    rota para upload de arquivo

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 interface User {
   name: string;
@@ -27,5 +32,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
